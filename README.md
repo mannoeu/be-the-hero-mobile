db7f4a806571163fe6c4199052099a6bb400f303
MANN03U <emmanuel_costasilva@hotmail.com>
2020-03-28 17:21:34 -0300

    mobile da aplicação

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Be The Hero</h1>
+<p align="center">🚀 OmniStack Week 11.0</p>
+
+## About
+The mobile version of a platform that aims to connect people who want to help NGOs. 🚀 OmniStack Week 11.0
---
+++ src/pages/Incidents/index.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import api from "../../services/api";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
+import styles from "./styles";
+import logoImg from "../../assets/logo.png";
+
+export default function Incidents() {
+  const [incidents, setIncidents] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1); // começa na página um
+  const [loading, setLoading] = useState(false);
+
+  const navigation = useNavigation();
+
+  function navigateToDetail(incident) {
+    navigation.navigate("Detail", { incident });
+  }
+
+  async function loadIncidents() {
+    if (loading) {
+      return; // n busca
+    }
+    if (total > 0 && incidents.lenght === total) {
+      return; // n busca
+    }
+    setLoading(true);
+
+    const res = await api.get("incidents", {
+      params: { page } // número da página
+    });
+    setIncidents([...incidents, ...res.data]); // não sobrescreve o valor, apenas anexa
+    setTotal(res.headers["x-total-incidents"]);
+
+    setLoading(false);
+    setPage(page + 1);
+  }
+
+  useEffect(() => {
+    loadIncidents();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Total de <Text style={styles.headerTextBold}>{total} casos</Text>.
+        </Text>
+      </View>
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text style={styles.description}>
+        Escolha um dos casos abaixo e salve o dia
+      </Text>
+
+      <FlatList
+        data={incidents}
+        style={styles.incidentList}
+        keyExtractor={incident => String(incident.id)}
+        showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents} // quando chegar no final da lista chamar loadIncidents
+        onEndReachedThreshold={0.2} // qnts % do final da lista precisa estar para carregar novos itens (0% a 1%)
+        renderItem={({ item: incident }) => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>{incident.name}</Text>
+
+            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentValue}>{incident.description}</Text>
+
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentValue}>
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL"
+              }).format(incident.value)}
+            </Text>
+
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={() => navigateToDetail(incident)}
+            >
+              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Feather name="arrow-right" size={16} color="#e02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
